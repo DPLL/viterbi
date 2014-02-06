6bc8f47cf8e437b3a90efd19b2b1c0f4f0981563
David Gao <dg.gaoyunlong@gmail.com>
2014-02-06 17:53:46 -0600

    add Vertex.java and GraphGenerator.java

---
+++ codes/HMM/src/GraphGenerator.java
@@ -0,0 +1,7 @@
+
+public class GraphGenerator {
+	
+	public static void main(String [] args) {
+		
+	}
+}
---
+++ codes/HMM/src/Vertex.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+/**
+ * Vertex defines the properties of a vertex used in a random graph
+ *
+ * @author Yunlong Gao
+ * @since Feb 06, 2014
+ */
+
+public class Vertex {
+	int vertexID;
+	int wordNum;
+	String[] wordList;
+	
+
+	
+	public Vertex (int id, int numberOfWords, String[] listOfWords) 
+	{
+		vertexID = id;
+		wordNum = numberOfWords;
+		if (numberOfWords != listOfWords.length) {
+			System.out.println("Number of words does not match!!!");
+			System.exit(-1);
+		}
+		wordList = listOfWords;
+	}
+	
+	public Vertex (int id) 
+	{
+		this(id, 0, null);
+	}
+	
+	public Vertex() 
+	{
+		this(0);
+	}
+	
+	public String toString() 
+	{
+		return "vertexID("+ vertexID + ");  "
+				+ "wordNum(" + wordNum + ");  " + "wordList(" 
+				+ Arrays.toString(wordList) +")"; }
+	
+	public static void main(String args[]) 
+	{
+		Vertex v = new Vertex(1, 2, new String[]{"hi", "hello"});
+		System.out.println("vertex v is: "+ v);
+	}
+}
