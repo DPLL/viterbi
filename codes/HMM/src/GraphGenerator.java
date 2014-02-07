4e3d7b156c47c5b80da2844456ddd8a4f0f1b20b
David Gao <dg.gaoyunlong@gmail.com>
2014-02-06 21:29:48 -0600

    GraphGenerator.java

--- codes/HMM/src/GraphGenerator.java
+++ codes/HMM/src/GraphGenerator.java
@@ -1,7 +1,37 @@
 
 public class GraphGenerator {
 	
-	public static void main(String [] args) {
+	// number of vertex
+	int numVertex = 15;
+	// number of edges
+	int numEdge;
+	// density of the DAG
+	double density;
+	// number of words per node
+	int wordPerNode;
+	// length of word
+	int wordLength;
+	
+	public static void graphGen() 
+	{
 		
+	}
+	
+	public static void main(String [] args) 
+	{
+
+		/*
+		if (args.length != 3) {
+			System.out.println("Correct Usage: ./GraphGenerator [density] [wordPerNode] [wordLength]");
+			System.exit(-1);
+		}
+		
+		density = Double.parseDouble(args[0]);
+		wordPerNode = Integer.parseInt(args[1]);
+		wordLength = Integer.parseInt(args[2]);
+		// cast numEdge to integer
+		numEdge = (int) (density * numVertex * (numVertex-1));
+			
+		*/
 	}
 }
